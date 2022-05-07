0b1fef1951cd8c14f4e83ab5bc93e63309c54b23
abhay734 <vermaabhay734@gmail.com>
2022-05-07 12:39:32 +0530

    Add Card UI components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import ExpenseItem from './Components/ExpenseItem';
+import Expenses from './Components/Expenses';
 
 function App() {
   const expenses = [
@@ -23,22 +23,12 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+  
   return (
     <>
     <div>
       <h2>Lets get started. </h2>
-      <ExpenseItem title={expenses[0].title}
-      amount={expenses[0].amount}
-      date={expenses[0].date} />
-      <ExpenseItem title={expenses[1].title}
-      amount={expenses[1].amount}
-      date={expenses[1].date} />
-      <ExpenseItem title={expenses[2].title}
-      amount={expenses[2].amount}
-      date={expenses[2].date} />
-      <ExpenseItem title={expenses[3].title}
-      amount={expenses[3].amount}
-      date={expenses[3].date} />
+      <Expenses items={expenses}/>
     </div>
     </>
   );

758b1eac9559696bf7a11de37cbe8e6fc18742fb
abhay734 <vermaabhay734@gmail.com>
2022-06-09 13:32:27 +0530

    Organising files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import Expenses from './Components/Expenses';
+import React from 'react';
+import Expenses from './Components/Expenses/Expenses';
 
 function App() {
   const expenses = [
--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,8 +1,12 @@
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
-import Card from "./UI/Card";
+import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+  function Title() {
+      console.log("Clicked!!");
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +14,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">Rs{props.amount}</div>
       </div>
+      <button onClick={Title} >Change Title</button>
     </Card>
   );
 }

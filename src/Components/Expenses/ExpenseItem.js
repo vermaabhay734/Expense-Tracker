730ed9bdeb6cb2e9c148fd2088d5a02abc2b645b
abhay734 <vermaabhay734@gmail.com>
2022-06-29 10:09:34 +0530

    Adding new expense dynamically

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  function Title() {
-      console.log("Clicked!!");
-  }
+  //function clickHandler() {}
+  const [title,setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('Updated!');
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">Rs{props.amount}</div>
       </div>
-      <button onClick={Title} >Change Title</button>
+      <button onClick={clickHandler} >Change Title</button>
     </Card>
   );
 }

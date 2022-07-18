a0940ff779162eb79dc53609db5924d5139c3991
abhay734 <vermaabhay734@gmail.com>
2022-07-18 08:23:42 +0530

    Handling Form Submission

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -2,53 +2,71 @@ import React, {useState} from "react";
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-    // const [enteredTitle, setEnteredTitle] = useState('');
-    // const [enteredAmount, setEnteredAmount] = useState('');
-    // const [enteredDate, setEnteredDate] = useState('');
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
 
-    const [userInput, setUserInput] = useState({
-        enteredTitle: '',
-        enteredAmount: '',
-        enteredDate: ''
-    });
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: ''
+    // });
 
     const titleChangeHandler = (event) => {
-        // setEnteredTitle(event.target.value);
-        setUserInput({
-            ...userInput, // used for prevent the other data lost like amount and date
-            enteredTitle : event.target.value, //overwrite on above input.
-        })
+        setEnteredTitle(event.target.value);
+        // setUserInput({
+        //     ...userInput, // used for prevent the other data lost like amount and date
+        //     enteredTitle : event.target.value, //overwrite on above input.
+        // })
+        // setUserInput((prevState) => {
+        //     return{...prevState, enteredTitle: event.target.value};
+        // });
     };
 
     const amountChangeHandler = (event) => {
-        // setEnteredAmount(event.target.value);
-        setUserInput({
-            ...userInput,   // used for prevent the other data lost like title and date
-            enteredAmount : event.target.value,
-        })
+        setEnteredAmount(event.target.value);
+        // setUserInput({
+        //     ...userInput,   // used for prevent the other data lost like title and date
+        //     enteredAmount : event.target.value,
+        // })
     };
 
     const dateChangeHandler = (event) => {
-        // setEnteredDate(event.target.value);
-        setUserInput({
-            ...userInput,
-            enteredDate : event.target.value,
-        })
+        setEnteredDate(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate : event.target.value,
+        // })
     };
 
-    return <form>
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+        
+        //Clearing the form after submission.
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    };
+
+    return <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type='text' onChange={titleChangeHandler}/>
+                <input type='text' value={enteredTitle} onChange={titleChangeHandler}/>
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type='number' min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                <input type='number' min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler}/>
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type='date' min="2022-01-01" max="2025-12-31" onChange={dateChangeHandler} />
+                <input type='date' min="2022-01-01" max="2025-12-31" value={enteredDate} onChange={dateChangeHandler} />
             </div>
         </div>
         <div className="new-expense__actions">
